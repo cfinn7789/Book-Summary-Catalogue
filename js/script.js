5b4914cbb6bcb5f8e4948d65f0ab9438127f8321
cfinn7789 <cfinn7789@gmail.com>
2023-07-26 20:06:35 -0400

    removed unecessary page and its respective button

--- js/script.js
+++ js/script.js
@@ -6,9 +6,5 @@ $( "#favorites-btn" ).on("click", function(event) {
 	document.location.replace('./favorites.html');
 }); // Clicking the button takes you to the favorites page
 
-$( "#list-btn" ).on("click", function(event) {
-	document.location.replace('./list.html');
-}); // Clicking the button takes you to the list of books page
-
 
 // To Do: Learn how to create an API so that the summaries can be called when needed (reduces load on page)
